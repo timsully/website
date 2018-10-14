6d9a72627fcc4b9d165fef2306ffeb658be35dac
Tim Sullivan <timsullivan97@gmail.com>
2018-10-13 18:12:48 -0700

    adding JS for header to trigger color/transparency

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() > 70) {
+    $(".navbar-fixed-top").addClass("active");
+  } else {
+    //remove the background property so it comes transparent again (defined in your css)
+    $(".navbar-fixed-top").removeClass("active");
+  }
+});
